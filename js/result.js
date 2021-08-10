072f02c1a6114bbdd6ebbcb940a8a906385d9fea
Lee SeungJu <rebon1848@gmail.com>
2021-08-10 11:22:48 +0900

    result 수정

--- js/result.js
+++ js/result.js
@@ -1,6 +1,5 @@
 const viewBtn = document.querySelector('.view-btn');
 
-
 const viewAllTypeClickEvnet = (e) => {
     const domBody = document.querySelector('body');
     const viewWrap = document.querySelector('.view-wrap');
@@ -22,5 +21,53 @@ const viewAllTypeClickEvnet = (e) => {
     })
 
 }
+
+function createElements() {
+    const resultMbtiSubTitle = document.querySelector('.result-mbti-sub-title');
+    const resultBoxSubTitle = document.querySelector('.result-box-sub-title');
+    const resultMbtiTitle = document.querySelector('.result-mbti-title');
+    const resultBoxTitle = document.querySelector('.result-box-title');
+    const resultBoxText = document.querySelector('.reulst-box-text');
+    const resultMbtiImg = document.querySelector('.result-mbti-img');
+    const goodText = document.querySelector('.good-text');
+    const goodImg = document.querySelector('.good-img');
+    const badText = document.querySelector('.bad-text');
+    const badImg = document.querySelector('.bad-img')
+    const viewTypeItemBox = document.querySelector('.view-type-item-box')
+
+    resultMbtiTitle.innerHTML = userMbti;
+    resultBoxTitle.innerHTML = userMbti;
+
+    mbti.map(mbti => {
+        const { name, title, text, imgeGif, good, bad } = mbti;
+
+        if (name === userMbti) {
+            resultMbtiSubTitle.innerHTML = title;
+            resultBoxSubTitle.innerHTML = title;
+            resultBoxText.innerText = text;
+            resultMbtiImg.src = imgeGif;
+            goodText.innerHTML = good
+            goodImg.src = "/mbti/img/mbti-img/"+good+".gif"
+            badText.innerHTML = bad
+            badImg.src = "/mbti/img/mbti-img/"+bad+".gif"
+        }
+        
+        const mbtisList = document.createElement("li");
+        const mbtisLink = document.createElement('a');
+        const mbtisImg = document.createElement('img');
+        const mbtisText = document.createElement('p');
+
+        mbtisLink.setAttribute('href', "" + name.toLowerCase() + ".html");
+        mbtisText.innerText = name;
+        mbtisImg.src = imgeGif;
+        
+        viewTypeItemBox.appendChild(mbtisList);
+        mbtisList.appendChild(mbtisLink);
+        mbtisLink.appendChild(mbtisImg);
+        mbtisLink.appendChild(mbtisText)
+    })
+}
+
+createElements();
 
 viewBtn.addEventListener('click', viewAllTypeClickEvnet)
